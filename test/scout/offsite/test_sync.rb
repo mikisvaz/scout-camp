33fe2e9920abf4d872d631273fc6ca722474f1ab
Miguel Vazquez <mikisvaz@gmail.com>
2025-04-10 20:11:23 +0200

    nil

--- test/scout/offsite/test_sync.rb
+++ test/scout/offsite/test_sync.rb
@@ -1,6 +1,7 @@
 require File.expand_path(__FILE__).sub(%r(/test/.*), '/test/test_helper.rb')
 require File.expand_path(__FILE__).sub(%r(.*/test/), '').sub(/test_(.*)\.rb/,'\1')
 
+require 'scout'
 class TestSync < Test::Unit::TestCase
   def test_sync
     TmpFile.with_path do |tmpdir|
