81dd1189bea0b2f94928148eefc654617bedce04
Miguel Vazquez <mikisvaz@gmail.com>
2025-04-24 00:00:39 +0200

    Stable lambda_function for serving jobs

--- share/aws/lambda_function.rb
+++ share/aws/lambda_function.rb
@@ -4,20 +4,20 @@ def lambda_handler(event:, context:)
   Path.path_maps[:bucket] = "s3://#{ENV["AWS_BUCKET"]}/{TOPLEVEL}/{SUBPATH}"
   Path.path_maps[:default] = :bucket
 
-  require 'scout/workflow'
-  require 'scout/aws/s3'
-
   TmpFile.tmpdir = Path.setup('/tmp')
   Open.sensible_write_dir = Path.setup('/tmp/sensible_write')
 
+  require 'scout/workflow'
+  require 'scout/aws/s3'
+
+
   Log.info "Payload: #{Log.fingerprint(event)}"
 
-  workflow, task_name, jobname, inputs, clean, queue, info = IndiferentHash.process_options event,
-    :workflow, :task_name, :jobname, :inputs, :clean, :queue, :info
+  workflow, task_name, jobname, inputs, clean, queue, info, path = IndiferentHash.process_options event,
+    :workflow, :task_name, :jobname, :inputs, :clean, :queue, :info, :path
 
+  task_name = "path" if path
   raise ParameterException, "No workflow specified" if workflow.nil?
-
-  workflow = Workflow.require_workflow workflow
 
   case task_name
   when nil
@@ -28,7 +28,17 @@ def lambda_handler(event:, context:)
   else
     Workflow.job_cache.clear
 
-    job = workflow.job(task_name, jobname, inputs)
+    if path
+      job = Step.load path
+      task_name = job.task_name
+      workflow = job.workflow
+      wait = true
+    else
+      workflow = Workflow.require_workflow workflow
+      job = workflow.job(task_name, jobname, inputs)
+    end
+
+    Log.info "Job info: #{job.info}"
 
     case clean
     when true, 'true'
@@ -37,29 +47,41 @@ def lambda_handler(event:, context:)
       job.recursive_clean
     end
 
-    if info
-      info = job.info.dup
-      info["path"] = job.path
-      info
-    elsif job.done?
-      job.load
-    elsif job.error?
-      raise job.exception
-    elsif job.started?
-      {
-        statusCode: 202,
-        body: job.path
-      }
-    elsif queue
-      save_inputs = Scout.var.queue[workflow.to_s][task_name][job.name].find :bucket
-      job.save_input_bundle(save_inputs) unless save_inputs.exists?
-      {
-        statusCode: 202,
-        body: job.path
-      }
-    else
-      job.produce
-      job.load
+    begin
+      if info
+        info = job.info.dup
+        info["path"] = job.path
+        {info: info}
+      elsif job.done?
+        job.load_info unless job.status == :done
+        {result: job.load}
+      elsif job.error?
+        {exception: job.exception, error: job.exception.message}
+      elsif job.started?
+        {job: job.path, info: job.info, status: job.info[:status] }
+      elsif queue
+        save_inputs = Scout.var.queue[workflow.to_s][task_name][job.name].find :bucket
+
+        job.save_info status: :queue 
+        job.save_input_bundle(save_inputs) unless save_inputs.exists?
+
+        Log.info "Queue: #{save_inputs}"
+
+        {job: job.path}
+      elsif wait
+        save_inputs = Scout.var.queue[workflow.to_s][task_name][job.name].find :bucket
+        if not save_inputs.exists?
+          job.join
+          raise TryAgain 
+        else
+          {job: job.path}
+        end
+      else
+        job.produce
+        raise TryAgain
+      end
+    rescue TryAgain
+      retry
     end
   end
 end
